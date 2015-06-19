67000c0bb3aaaa61521a9bf5f13decc12cf60259
baba-beda <darya.nightmare@gmail.com>
2015-06-19 20:28:33 +0300

    i'm too exhausted to finish T :(

--- T/T.java
+++ T/T.java
@@ -10,51 +10,74 @@ public class T {
 
     public void solve() throws IOException {
         int tests = in.nextInt();
+
+        long[] powersTen = new long[17];
+        powersTen[0] = 1;
+        long[] powersNine = new long[17];
+        powersNine[0] = 1;
+
+        long[][] combinations = new long[17][17];
+
+        for (int i = 1; i < 17; i++) {
+            powersNine[i] = powersNine[i - 1] * 9;
+            powersTen[i] = powersTen[i - 1] * 10;
+            combinations[i][i] = 1;
+            for (int j = i + 1; j < 17; j++) {
+                combinations[i][j] = combinations[i][j - 1] * j / (j - i);
+            }
+        }
+
+
         for (int i = 0; i < tests; i++) {
             long[] digits = new long[10];
-            for (int j = 0; j < 10; j++) {
-                digits[j] = in.nextLong();
-            }
-
-            long min = digits[0];
-            for (int j = 1; j < 10; j++) {
-                if (digits[j] < min) {
-                    min = digits[j];
-                }
-            }
 
             long ans = 0;
-            while (true) {
-                long p = 1;
-                long numberOfDigits = 1;
-                long numberOfZeros = 0;
-                int power = 0;
-
-                long toSubtract = 0;
-                while (min >= numberOfDigits + numberOfZeros) {
-                    toSubtract += numberOfDigits + numberOfZeros;
-                    min -= toSubtract;
-                    numberOfDigits = numberOfDigits * 9 + 9;
-                    numberOfZeros = numberOfZeros * 9 + 9;
-                    p *= 10;
-                    power++;
-                }
-
-                for (int j = 0; j < 10; j++) {
-                    digits[j] -= toSubtract;
-                }
-
-                if (digits[0] >= power && digits[1] > 0) {
-                    ans += p;
-                } else {
-                    ans += p - 1;
+            boolean changed = false;
+            for (int j = 0; j < 10; j++) {
+                digits[j] = in.nextLong();
+                if (digits[j] == 0) {
+                    ans = j > 0 ? j - 1 : 0;
+                    changed = true;
                     break;
                 }
             }
+            if (changed) {
+                out.println(ans);
+                continue;
+            }
 
+            long used = 0;
+            int length = 2;
+            for (; length < 17; length++) {
+                int[] result = new int[length];
+                for (int rad = length - 1; rad >= 0; rad--) {
 
+                    for (int j = rad == length - 1 ? 1 : 0; j < 10; j++) {
+                        long needed = 0;
+                        for (int l = 1; l < rad; l++) {
+                            needed += combinations[l][rad] * powersNine[rad - l];
+                        }
+                        if (j > 1) {
+                            needed++;
+                        }
 
-            out.println(ans);
+                        for (int k = 0; k < j; k++) {
+                            if (digits[k] < needed + used) {
+                                result[rad] = j;
+                                break;
+                            }
+                        }
+                        if (result[rad] == 0) {
+                            used += needed;
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                }
+
+            }
+
         }
     }
 
